e259111f132a414901f29af6f2f538c2daaa09e8
volha <v.khviadchenia@gmail.com>
2021-09-08 05:49:10 +0200

    feat: add and fill element Total

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let fragment = new DocumentFragment();
+const fragment = new DocumentFragment();
 
 // Add sounds
 const moved = document.createElement('audio');
@@ -42,12 +42,55 @@ readyImage.setAttribute('src', './Assets/ready image.png');
 readyImage.classList.add('ready-image');
 templateImage.appendChild(readyImage);
 
-
 templateContainer.append(templateTitle, templateImage);
 
 const templateParagraph = document.createElement('div');
 templateParagraph.classList.add('template-paragraph');
 templateParagraph.innerHTML = 'How it should be';
 templateTitle.appendChild(templateParagraph);
+
+// Fill gameContainer
+const total = document.createElement('div');
+total.classList.add('total');
+gameContainer.appendChild(total);
+
+// Create button
+let reset = document.createElement('button');
+reset.classList.add('reset');
+reset.textContent = 'Reset';
+total.appendChild(reset);
+reset.addEventListener('click', mix);
+
+// Create function for remix elements
+function mix() {
+  location.reload();
+}
+
+// Create time
+let time = document.createElement('form');
+time.setAttribute('name', 'MyForm');
+time.classList.add('time');
+total.appendChild(time);
+time.textContent = 'Time: ';
+
+// Create timer
+let input = document.createElement('input');
+input.name = 'stopwatch';
+input.size = '5';
+input.value = "00:00:00";
+input.disabled = true;
+input.classList.add('input');
+time.appendChild(input);
+
+// Create score
+let score = document.createElement('div');
+score.classList.add('score');
+total.appendChild(score);
+score.textContent = 'Score: ';
+let count = 0;
+
+
+
+
 
 document.body.append(fragment);
